68c52ab07235083fe82ca8d8d9907a40d67de452
Paulagomis <paulabailarina9@gmail.com>
2025-10-09 16:12:37 +0200

    feat: add function to download all analyses results for a project

--- scripts/api-utils.py
+++ scripts/api-utils.py
@@ -259,3 +259,57 @@ def download_file(url: str, output_dir: str):
         with open(f"{output_dir}/{file_name}", 'wb') as f:
             for chunk in r.iter_content(chunk_size=8192): 
                 f.write(chunk)
+
+
+def download_all_analyses_results(project_name: str, output_dir: Path, main_headers: dict[str, str]) -> None:
+    """Download all analyses results from a project.
+
+    Parameters
+    ----------
+    project_name : str
+        Name of the project to download the analyses from.
+    output_dir : Path
+        Directory to save the downloaded analyses.
+    main_headers : dict[str, str]
+        Headers for the API request.                    
+    Returns
+    -------     
+    None
+        The analyses are downloaded as zip files.
+    Raises
+    ------
+    requests.exceptions.HTTPError
+        If the request fails.
+    ValueError
+        If the project is not found.
+    """
+    # Get the project UUID from the project name
+    projects_list = get_all_projects(main_headers)["records"]
+    project_uuid = None
+    for project in projects_list:
+        if project["name"] == project_name:
+            project_uuid = project["uuid"]
+            break
+    if project_uuid is None:
+        raise ValueError(f"Project {project_name} not found")
+
+    print(f"Project {project_name} found with UUID {project_uuid}")
+
+    # Get all analyses from the project
+    analyses_list = get_all_analyses(project_uuid, main_headers)["records"]
+
+    # Download the result files for each analysis
+    skipped_analyses = []
+    for analysis in analyses_list:
+        if analysis["status"] != "DONE":
+            skipped_analyses.append(analysis)
+            continue
+        analysis_uuid = analysis["uuid"]
+        output_file = output_dir / f"{analysis_uuid}.zip"
+        get_analysis_result_files(project_uuid, analysis_uuid, main_headers, output_file)
+
+    # Print the skipped analyses
+    if skipped_analyses:
+        print("The following analyses were skipped because they are not completed or resulted in an error:")
+        for analysis in skipped_analyses:
+            print(f"- Analysis {analysis['uuid']} for patient {analysis['patient']['patient_id']} with status {analysis['status']}")
